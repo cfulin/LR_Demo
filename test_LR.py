4c31bcf815bdbbc1c5fefd7b1a60b6a45920e034
cfulin <740494415@qq.com>
2017-07-18 15:48:09 +0800

    修改#格式

--- test_LR.py
+++ test_LR.py
@@ -14,7 +14,7 @@ def loadData():
         train_y.append(float(lineArr[2]))
     return mat(train_x), mat(train_y).transpose()
 
-## step 1: load data
+# step 1: load data
 print "step 1: load data..."
 train_x, train_y = loadData()
 test_x = train_x
@@ -22,7 +22,7 @@ test_y = train_y
 # [20: 41] train_x = train_x train_y = train_y
 numSamples, numFeatures = shape(test_x)
 
-## step 2: training...
+# step 2: training...
 print "step 2: training..."
 opts = {'alpha': 0.01, 'maxIter': 100, 'optimizeType': 'smoothStocGradDescent'}
 opts['maxIter'] = input('please input maximum number of iterations: ')
@@ -31,7 +31,7 @@ optimalWeights = weights[0]
 # print weights[1]
 print ('optimal weights: ', optimalWeights)
 
-## step 3: testing
+# step 3: testing
 print "step 3: testing..."
 testResult = testLogRegres(optimalWeights, test_x, test_y)
 accuracy = testResult[0]
@@ -43,11 +43,11 @@ write_excel(test_x, score)
 #     print score[i]
 # print mat(sorted(score[1], reverse = True)).transpose()
 
-## step 4: show the result
+# step 4: show the result
 print "step 4: show the result..."
 print 'The classify accuracy is: %.3f%%' % (accuracy * 100)
 # showLogRegres(optimalWeights, train_x, train_y)
 
-## step 5: show weights curves
+# step 5: show weights curves
 print "step 5: show weights curves..."
 showWeightCurves(weights, opts)
